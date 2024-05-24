4464e3f1487775e06c0e9bf2d2f87e9a067cb8d0
dhruv01234 <1235dhruv@gmail.com>
2024-05-24 14:10:31 +0530

    added

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_iam_user" "example_user" {
+  name = "example-user"
+}
